cdffdf56c0d86ad7a57f8d13a1340a181dfe7dfc
Isaac Nadar <isaac.v.nadar@gmail.com>
2023-02-23 14:53:43 +0530

    added hero image

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import Banner from '../components/banner';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-
   const handleOnBannerBtnClick = () => {
     console.log('Hi banner button clicked');
-  }
+  };
 
   return (
     <>
@@ -21,7 +21,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerBtnClick} />
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.png" width={700} height={400} />
+        </div>
       </main>
     </>
   );
