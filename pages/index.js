fd8463cd1f9fb7175e40601f4607e0bb4c2fdc5c
Isaac Nadar <isaac.v.nadar@gmail.com>
2023-02-24 14:50:02 +0530

    created basic card component

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import Banner from '../components/banner';
 import Image from 'next/image';
+import Card from '@/components/card';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,6 +29,12 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+
+        <Card
+          name="Dope Coffee"
+          imgUrl="/static/hero-image.png"
+          href="/coffee-store/dope-coffee"
+        />
       </main>
     </>
   );
