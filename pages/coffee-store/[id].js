dbddf346cccdb9a185957a303315a1373ca20d64
Isaac Nadar <isaac.v.nadar@gmail.com>
2023-03-05 18:45:58 +0530

    save

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,13 +1,17 @@
 import { useRouter } from 'next/router';
 import coffeeStoresData from '@/data/coffee-stores.json';
+import Link from 'next/link';
+import Head from 'next/head';
+
+import styles from '@/styles/coffee-store.module.css';
+import Image from 'next/image';
 
 export const getStaticProps = async (staticProps) => {
   const params = staticProps.params;
-
   return {
     props: {
       coffeeStore: coffeeStoresData.find(
-        (coffeeStore) => coffeeStore.id === 0
+        (coffeeStore) => coffeeStore.id === parseInt(params.id, 10)
       ),
     },
   };
@@ -15,17 +19,57 @@ export const getStaticProps = async (staticProps) => {
 
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
 export const getStaticPaths = async (ctx) => {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return { params: { id: coffeeStore.id } };
+  });
+
   return {
     paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
-    fallback: "blocking"
+    fallback: true,
   };
 };
 
-const CoffeeStore = () => {
+const CoffeeStore = (props) => {
   const router = useRouter();
-  console.log('router', router);
 
-  return <div>Coffee store Page {router.query.id}</div>;
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
+
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link legacyBehavior href="/">
+              <a>back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            className={styles.storeImage}
+            src={imgUrl}
+            width={600}
+            height={360}
+            alt={name}
+          />
+        </div>
+
+        <div className={styles.col2}>
+          <p>{address}</p>
+          <p>{neighbourhood}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeStore;
