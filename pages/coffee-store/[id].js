a17c4d618d13607deb1c32453058e4c6704b8caf
Isaac Nadar <isaac.v.nadar@gmail.com>
2023-03-06 17:20:02 +0530

    static files done

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,20 @@
 import { useRouter } from 'next/router';
-import coffeeStoresData from '@/data/coffee-stores.json';
 import Link from 'next/link';
 import Head from 'next/head';
 
 import styles from '@/styles/coffee-store.module.css';
 import Image from 'next/image';
+import cls from 'classnames';
+import { fetchCoffeeStores } from '@/lib/coffee-stores';
 
 export const getStaticProps = async (staticProps) => {
   const params = staticProps.params;
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
     props: {
-      coffeeStore: coffeeStoresData.find(
-        (coffeeStore) => coffeeStore.id === parseInt(params.id, 10)
+      coffeeStore: coffeeStores.find(
+        (coffeeStore) => coffeeStore.id === params.id
       ),
     },
   };
@@ -19,12 +22,13 @@ export const getStaticProps = async (staticProps) => {
 
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
 export const getStaticPaths = async (ctx) => {
-  const paths = coffeeStoresData.map((coffeeStore) => {
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = coffeeStores.map((coffeeStore) => {
     return { params: { id: coffeeStore.id } };
   });
 
   return {
-    paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
+    paths,
     fallback: true,
   };
 };
@@ -36,7 +40,11 @@ const CoffeeStore = (props) => {
     return <div>Loading...</div>;
   }
 
-  const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
+  const { name, imgUrl, address, neighbourhood } = props.coffeeStore;
+
+  const handleUpvoteButton = () => {
+    console.log('handle upvote');
+  };
 
   return (
     <div className={styles.layout}>
@@ -48,7 +56,7 @@ const CoffeeStore = (props) => {
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
             <Link legacyBehavior href="/">
-              <a>back to home</a>
+              <a>← Back to home</a>
             </Link>
           </div>
           <div className={styles.nameWrapper}>
@@ -56,16 +64,54 @@ const CoffeeStore = (props) => {
           </div>
           <Image
             className={styles.storeImage}
-            src={imgUrl}
+            src={
+              imgUrl ||
+              'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+            }
             width={600}
             height={360}
             alt={name}
           />
         </div>
 
-        <div className={styles.col2}>
-          <p>{address}</p>
-          <p>{neighbourhood}</p>
+        <div className={cls('glass', styles.col2)}>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/nearMe.svg"
+                width="24"
+                height="24"
+                alt="near me"
+              />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+
+          {neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/places.svg"
+                width="24"
+                height="24"
+                alt="places"
+              />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+          )}
+
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/star.svg"
+              width="24"
+              height="24"
+              alt="star"
+            />
+            <p className={styles.text}>1</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
         </div>
       </div>
     </div>
